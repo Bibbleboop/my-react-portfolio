3b2dc48b8cf0baabb8df796c2225f9854caaec45
Bibbleboop <nicjah@hotmail.com>
2023-03-28 01:34:37 +0100

    footer initial

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer>
+            <h3>{props.title}  My Contacts </h3>
+            
+            <a rel="noopener no referrer" href="https://github.com/Bibbleboop"> My Github</a>
+            <a href="https://www.linkedin.com/in/bibsy-bibbleboopz-4b6933262/" class="LI"> * LinkedIn * </a>
+            <a href="https://www.instagram.com/bibsybibbleboopz/" class="ig"> * Insta * </a>
+         
+        </footer>
+    )
+}
+
+export default Footer;
+
