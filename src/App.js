b5c5fa415f920ef79130c9021fd6b05715b9f60b
Bibbleboop <nicjah@hotmail.com>
2023-03-28 01:31:53 +0100

    added projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React from 'react';
 // import { useRoutes } from "react-router-dom";
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
@@ -6,6 +6,8 @@ import Nav from './pages/Nav';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
+import Footer from './components/Footer/footer';
+import Portfolio from './pages/Projects/Projects';
 // import Counter from "./components/Counter";
 
 
@@ -21,11 +23,12 @@ function App() {
       <Route path="*" element={<Nav />} / >
       <Route path="/" element={<Home title={title} />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="./assets/portfolio" element={<Portfolio />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
 
     {/* <Counter /> */}
-
+    <Footer/>
 
     </>
         )
