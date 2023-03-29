6c6a319a01049cdf94e71dd2f045e8b9fc553f99
Bibbleboop <nicjah@hotmail.com>
2023-03-29 20:19:25 +0100

    updated to projects.json

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,45 +1,38 @@
+/* eslint-disable no-unused-vars */
+import {Link} from 'react-router-dom';
 import { useState } from "react";
-// import React from "react";
-import project1 from '../../assets/portfolio/JSF Just Search Films.png';
-import project2 from '../../assets/portfolio/Random Password Generator.png';
-import project3 from '../../assets/portfolio/Scheduler Planner.png';
-import project4 from '../../assets/portfolio/Bootstrap Portfolio.png';
+import React from "react";
+import project from './projects.json'
+import projectCard from '../../projectCard';
+import projectData from './projects.json'
+// import project1 from '../../assets/portfolio/JSF Just Search Films.png';
+// import project2 from '../../assets/portfolio/Random Password Generator.png';
+// import project3 from '../../assets/portfolio/Scheduler Planner.png';
+// import project4 from '../../assets/portfolio/Bootstrap Portfolio.png';
 // import project5 from '../../assets/portfolio/image5.jpg';
+// import projectData from '../Projects/projects.json';
 
 function Projects() {
-    let data=[
-        {
-            id: 1,
-            image: project1,
-            title: 'JSF Just Search Films',
-            github: 'https://github.com/Bibbleboop/Just-Search-Films'
-        },
-        {
-            id: 2,
-            image: project2,
-            title: 'Random Password Generator',
-            github: 'https://github.com/Bibbleboop/Password-Generator-Module-5-Challenge'
-        },
-        {
-            id: 3,
-            image: project3,
-            title: 'Scheduler Planner',
-            github: 'https://github.com/Bibbleboop/Daily-Planner-App'
-        },
-        {
-            id: 4,
-            image: project4,
-            title: 'Bootstrap Portfolio',
-            github: 'https://github.com/Bibbleboop/Bootstrap-Portfolio'
-        },
-        // {
-        //     id: 1,
-        //     image: project1,
-        //     title: 'JSF Just Search Films',
-        //     github: 'https://github.com/Bibbleboop/Just-Search-Films'
-        // },
-    ]
-};
+    // let [projects, ] = useState(projectData);
+    let [projects, setProjects] = useState(projectData);
+    return (
+        <div>
+        <container>
+            {projects.map((project) => (
+            <div className='item-container'>
+                {/* <Link to="/src/pages/Projects/Projects.js" */}
+            <projectCard>
+            id={project.id}
+            image={project.image}
+            title={project.title}
+            github={project.github} </projectCard> 
+            </div>
+            ))}
+
+        </container>
+        </div> );
+}
+
 
 
 
