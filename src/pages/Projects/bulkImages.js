bc2b9476ed6a071cf84c3dd27b8ca0d4f497954b
Bibbleboop <nicjah@hotmail.com>
2023-03-30 21:36:02 +0100

    bulk images

---
+++ src/pages/Projects/bulkImages.js
@@ -0,0 +1,16 @@
+import project1 from '../../assets/portfolio/JSF Just Search Films.png';
+import project2 from '../../assets/portfolio/Random Password Generator.png';
+import project3 from '../../assets/portfolio/Scheduler Planner.png';
+import project4 from '../../assets/portfolio/Bootstrap Portfolio.png';
+import project5 from '../../assets/portfolio/image5.jpg';
+
+let bulkProjects = [
+    ... project1,
+    ... project2,
+    ... project3,
+    ... project4,
+    ... project5,
+];
+
+
+export default bulkImages;
