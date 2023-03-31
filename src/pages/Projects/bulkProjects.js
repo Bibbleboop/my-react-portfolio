e663b848c11438c6ef34b8f4904d8f659543c2d1
Bibbleboop <nicjah@hotmail.com>
2023-03-31 03:14:11 +0100

    deprecated as renamed

---
+++ src/pages/Projects/bulkProjects.js
@@ -0,0 +1,16 @@
+import project1 from '../../images/screenshots/JSF-Just-Search-Films.png';
+import project2 from '../../images/screenshots/Random-Password-Generator.png';
+import project3 from '../../images/screenshots/Scheduler-Planner.png';
+import project4 from '../../images/screenshots/Bootstrap-Portfolio.png';
+import project5 from '../../images/screenshots/Eat-Rave-Love.png';
+
+let bulkProjects = [
+    ...project1,
+    ...project2,
+    ...project3,
+    ...project4,
+    ...project5,
+];
+
+
+export default bulkProjects;
