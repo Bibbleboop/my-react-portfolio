d98270d6fb1c48bc25b94ece70ae5db85442b9dc
Bibbleboop <nicjah@hotmail.com>
2023-04-03 02:19:06 +0100

    try function

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,21 +1,45 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 //image of projects
 import bulkProjects from './bulkProjects';
+import projectData from './projects.json'
+import projectCard from './projectCard';
 
-export default function projects() {
-    return (
-    <div className='bulkProjectsDisplay'>
-        {bulkProjects.map((img) => (
-        <img key={img} src={img} alt={img} className="bulkProject" />
-    
-    ))}
+function projects() {
+    let [projectsArray] = useState(projectData)
+
+    return
+    <div className='layout'>
+        <section className="projectsData"> 
+        {projectsArray.map(props) => (
+            <projectCard
+            id={props.id}
+            title={props.title}
+            image={props.image}
+            github={props.github}
+            />
+        ))
+        
+        </section>
     </div>
-    );
-}
+    
+};
+
+
+
+
+{/* // // export default function projects() { */}
+{/* //     return (
+//     <div className='bulkProjectsDisplay'>
+//         {bulkProjects.map((img) => ( */}
+{/* //         <img key={img} src={img} alt={img} className="bulkProject" />
+    
+//     ))}
+//     </div>
+//     );
+// } */}
 
 
 
 
 
-// export default Projects;
+export default projects;
